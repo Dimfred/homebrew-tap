5619fe594a181fc8ee20b1d7c630a886fc7cd87b
Dmitrij Vinokour <dmitrij.vinokour@cojodi.com>
2026-02-19 21:02:14 +0100

    Add tcg-lightning-dev cask

---
+++ Casks/tcg-lightning-dev.rb
@@ -0,0 +1,20 @@
+cask "tcg-lightning-dev" do
+  version "0.1.28-rc1"
+  sha256 "PLACEHOLDER"
+
+  url "https://github.com/Dimfred/tcg-lightning/releases/download/v#{version}/macos-tcg-lightning_#{version}_aarch64.dmg"
+  name "TCG Lightning (Dev)"
+  desc "AI Powered Deck Building App - Development Release"
+  homepage "https://github.com/Dimfred/tcg-lightning"
+
+  depends_on macos: ">= :monterey"
+  depends_on arch: :arm64
+
+  app "tcg-lightning.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.tcg-lightning-dev.desktop",
+    "~/Library/Caches/com.tcg-lightning-dev.desktop",
+    "~/Library/Preferences/com.tcg-lightning-dev.desktop.plist",
+  ]
+end
