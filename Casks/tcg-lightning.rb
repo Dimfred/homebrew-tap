33651f1837bc89947f538e3258dc285c451d0e1b
Dmitrij Vinokour <vinokour.dmitrij@gmail.com>
2026-03-13 13:30:46 +0100

    tcg-lightning: bump to 0.1.34

--- Casks/tcg-lightning.rb
+++ Casks/tcg-lightning.rb
@@ -1,6 +1,6 @@
 cask "tcg-lightning" do
-  version "0.1.33"
-  sha256 "2f484a866c36ee7a730cd0d793fa91147df6dfb24ba24d1f1dd5aa6d1830548e"
+  version "0.1.34"
+  sha256 "ae0c6b6f372cf5940c5f9bd1bdfb85c24960cfa88b94ab6eb41b3eea9b07c5d2"
 
   url "https://github.com/Dimfred/tcg-lightning/releases/download/v#{version}/macos-tcg-lightning_#{version}_aarch64.dmg"
   name "TCG Lightning"
