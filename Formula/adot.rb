f08a5bb074f0ba132f28b2b44ad4307af63dcb5b
Dmitrij Vinokour <vinokour.dmitrij@gmail.com>
2026-04-03 13:43:37 +0200

    Update adot to v0.1.9

--- Formula/adot.rb
+++ Formula/adot.rb
@@ -1,20 +1,20 @@
 class Adot < Formula
   desc "A minimal dotfile manager"
   homepage "https://github.com/Dimfred/adot"
-  version "0.1.8"
+  version "0.1.9"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Dimfred/adot/releases/download/v0.1.8/macos-adot-v0.1.8-arm64"
-      sha256 "5c053c240f5a60d05d9fca00209cb862f6a3ee2b7f42131ace80d909ac4fe78d"
+      url "https://github.com/Dimfred/adot/releases/download/v0.1.9/macos-adot-v0.1.9-arm64"
+      sha256 "f698880e18c968e9cf224ec0fe7640ced2d2852879da9802fef414e605e0cb5a"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/Dimfred/adot/releases/download/v0.1.8/linux-adot-v0.1.8-x86_64"
-      sha256 "8a206a167c6165a4aafbe506de9c422555fae9b83a263724b12d5b76cd842009"
+      url "https://github.com/Dimfred/adot/releases/download/v0.1.9/linux-adot-v0.1.9-x86_64"
+      sha256 "664f74022d2f8e73c625fc44a026ff052c42244732c3cf97d8c1ad91c111d249"
     end
   end
 
