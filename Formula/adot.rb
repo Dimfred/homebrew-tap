b33d6131df79b2f4a346eb46f19a585bb7482022
Dmitrij Vinokour <vinokour.dmitrij@gmail.com>
2026-04-03 13:16:29 +0200

    Update adot to v0.1.2

--- Formula/adot.rb
+++ Formula/adot.rb
@@ -1,7 +1,7 @@
 class Adot < Formula
   desc "A minimal dotfile manager"
   homepage "https://github.com/Dimfred/adot"
-  url "https://github.com/Dimfred/adot/archive/refs/tags/v0.1.1.tar.gz"
+  url "https://github.com/Dimfred/adot/archive/refs/tags/v0.1.2.tar.gz"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
